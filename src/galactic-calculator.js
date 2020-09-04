cae006bcdbcb909969d4afec155a94b9238ca538
donnymays <dtmays2@gmail.com>
2020-09-04 11:49:47 -0700

    working on ui logic

--- src/galactic-calculator.js
+++ src/galactic-calculator.js
@@ -9,50 +9,50 @@ export class Age {
   constructor(yearsOnEarth, lifeExpectancy) {
     this.yearsOnEarth = yearsOnEarth;
     this.lifeExpectancy = lifeExpectancy;
-  };
+  }
 
   mercuryAge() {
     return Math.round(this.yearsOnEarth / yearsOnMercury);
-  };
+  }
 
   venusAge() {
     return Math.round(this.yearsOnEarth / yearsOnVenus);
-  };
+  }
   
   marsAge() {
     return Math.round(this.yearsOnEarth / yearsOnMars);
-  };
+  }
 
   jupiterAge() {
     return Math.round(this.yearsOnEarth / yearsOnJupiter);
-  };
+  }
 
   earthLifeLeft() {
     return Math.round(this.lifeExpectancy - this.yearsOnEarth);
-  };
+  }
 
   mercuryLifeLeft() {
     return Math.round((this.lifeExpectancy - this.yearsOnEarth) / yearsOnMercury);
-  };
+  }
 
   venusLifeLeft() {
     return Math.round((this.lifeExpectancy - this.yearsOnEarth) / yearsOnVenus);
-  };
+  }
   
   marsLifeLeft() {
     return Math.round((this.lifeExpectancy - this.yearsOnEarth) / yearsOnMars);
-  };
+  }
 
   jupiterLifeLeft() {
     return Math.round((this.lifeExpectancy - this.yearsOnEarth) / yearsOnJupiter);
-  };
+  }
 
   yearsOverAchieving() {
     if (this.yearsOnEarth > this.lifeExpectancy) {
       return this.yearsOnEarth - this.lifeExpectancy;
-    };
-  };
-};
+    }
+  }
+}
 
 
 
--- src/main.js
+++ src/main.js
@@ -2,11 +2,25 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import { Age } from "./../src/galactic-calculator.js"
+import { Age } from "./../src/galactic-calculator.js";
 
 $(document).ready(function() {
   $("#galacticForm").submit(function(event) {
     event.preventDefault();
+
+    let ageInput = parseInt($("#ageInput").val());
+    let ageOnMercury = Age.prototype.mercuryAge(ageInput); 
+    let ageOnVenus = Age.prototype.venusAge(ageInput); 
+    let ageOnMars = Age.prototype.marsAge(ageInput); 
+    let ageOnJupiter = Age.prototype.jupiterAge(ageInput); 
+
+    $("span#mercuryAge").text(ageOnMercury);
+    $("span#venusAge").text(ageOnVenus);
+    $("span#marsAge").text(ageOnMars);
+    $("span#jupiterAge").text(ageOnJupiter);
+   
+  
+    
 
   });
 });
